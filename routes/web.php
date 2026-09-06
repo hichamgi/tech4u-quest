a01d9cf8eec1c535d13a5425675897242cd3ac92
hichamgi <13285605+hichamgi@users.noreply.github.com>
2026-09-06 19:43:04 +0100

    Ajout des routes MVC élève et jeu

--- routes/web.php
+++ routes/web.php
@@ -2,11 +2,21 @@
 declare(strict_types=1);
 
 use App\Controllers\AuthController;
+use App\Controllers\GameController;
 use App\Controllers\HomeController;
+use App\Controllers\StudentController;
 use App\Core\Router;
 
 /** @var Router $router */
 $router->get('/', [HomeController::class, 'index']);
+
 $router->get('/login', [AuthController::class, 'login']);
 $router->post('/login', [AuthController::class, 'login']);
 $router->get('/logout', [AuthController::class, 'logout']);
+
+$router->get('/dashboard', [StudentController::class, 'dashboard']);
+$router->get('/module/{id}', [StudentController::class, 'module']);
+$router->post('/module/{id}/start', [StudentController::class, 'startModule']);
+
+$router->get('/question/{attempt}', [GameController::class, 'question']);
+$router->post('/question/{attempt}', [GameController::class, 'question']);
